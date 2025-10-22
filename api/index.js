1d46c355c666da03098402bcfff3bd9f7d15cc58
Vatukah <anupamthakur058@gmail.com>
2025-10-22 15:39:40 +0530

    fry

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 import express from 'express';
 
-const app = express();
+export const app = express();
 app.get('/api/hello', (req, res) => {
   res.status(200).json({ message: 'Hello from the serverless function!' });
 });
 
-module.exports = app;
